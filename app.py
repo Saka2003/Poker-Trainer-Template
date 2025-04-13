c65883c73cb37380b1428c26a424ae37b00b6ecc
Saka2003 <savirneni03@gmail.com>
2025-04-13 12:19:27 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+from logic.trainer import get_hand_scenario, evaluate_action
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/new_hand', methods=['GET'])
+def new_hand():
+    mode = request.args.get('mode', 'response')
+    scenario = get_hand_scenario(mode)
+    return jsonify(scenario)
+
+@app.route('/evaluate', methods=['POST'])
+def evaluate():
+    data = request.json
+    result = evaluate_action(data['position'], data.get('opener'), data['hand'], data['action'])
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
